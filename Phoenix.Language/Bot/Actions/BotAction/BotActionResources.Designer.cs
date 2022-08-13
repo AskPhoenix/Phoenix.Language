91681768ffd5f2b496851bbc944af80d4f27981d
Théophile Spyrou <theofilos.spyrou@gmail.com>
2022-08-13 14:10:50 +0300

    Move Bot Action Resources

--- Phoenix.Language/Bot/Actions/BotAction/BotActionResources.Designer.cs
+++ Phoenix.Language/Bot/Actions/BotAction/BotActionResources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Phoenix.Language.Types {
+namespace Phoenix.Language.Bot.Actions {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Phoenix.Language.Types {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Phoenix.Language.Types.BotAction.BotActionResources", typeof(BotActionResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Phoenix.Language.Bot.Actions.BotAction.BotActionResources", typeof(BotActionResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
